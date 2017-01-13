945542041b827212c947e2b112ba2dfac2fa6cdb
Ichiato Ikikin <ichiatoikikin@gmail.com>
2017-01-13 15:36:15 +0100

    delete rooms.js

--- JScript/getRoom.js
+++ JScript/getRoom.js
@@ -1,3 +1,34 @@
+var meetingRoom = {
+  'AWD1': 'Avialable',
+  'AWD2': 'Not Avialable',
+  'AWD3': 'Not Avialable',
+  'AWD4': 'Not Avialable',
+}
+var learnRoom = {
+  'AWD1': 'Avialable',
+  'AWD2': 'Avialable',
+  'AWD3': 'Not Avialable',
+  'AWD4': 'Not Avialable',
+}
+var gamesRoom = {
+  'AWD1': 'Avialable',
+  'AWD2': 'Avialable',
+  'AWD3': 'Not Avialable',
+  'AWD4': 'Avialable',
+}
+var quietTime = {
+  'AWD1': 'Avialable',
+  'AWD2': 'Not Avialable',
+  'AWD3': 'Not Avialable',
+  'AWD4': 'Avialable',
+}
+var workingRoom = {
+  'AWD1': 'Avialable',
+  'AWD2': 'Avialable',
+  'AWD3': 'Not Avialable',
+  'AWD4': 'Not Avialable',
+}
+
 var getRoom = function (room) {
   if (room === 'meeting room') {
     return meetingRoom;
@@ -9,4 +40,9 @@ var getRoom = function (room) {
     return quietTime;
   } else if (room === 'working room') {
     return workingRoom;
+  } else {
+    return `No room named ${room}`
   }
+};
+
+getRoom ('meeting room')
