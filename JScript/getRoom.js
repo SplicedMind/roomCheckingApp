2200be63a3d075a873c7bb6a45458f1315aa50b3
Akinbode Femi <femi661@gmail.com>
2017-01-14 08:21:00 +0100

    Script made responsive

--- JScript/getRoom.js
+++ JScript/getRoom.js
@@ -1,4 +1,7 @@
-function getRoom(room) {
+function getRoom() {
+ 
+  var rooms = document.getElementById('s').value;
+   alert('getRoom Successfully Called ' + rooms);
   var meetingRoom = {
   'Meeting Room 1': 'Avialable',
   'Meeting Room 2': 'Not Avialable',
@@ -31,21 +34,34 @@ var workingRoom = {
   'Working Room 4': 'Not Avialable',
   'Working Room 5': 'Avialable'
 };
-
-  if (room === 'meeting room') {
-    document.getElementById('d').innerHTML = meetingRoom.toString();
+ 
+  if ('meetingRoom' === rooms) {
+    alert('Meeting Room Selected');
+    document.getElementById('d').innerHTML =  'Meeting Room 1: Avialable'
+    + '</br>Meeting Room 2: Avialable </br> Meeting Room 3: Not Avialable </br>'
+    + 'Meeting Room 5: Avialable </br> Meeting Room 6: Not Avialable';
     return true;
-  } else if (room === 'learning room') {
-    return learnRoom;
-  }else if (room === 'games room') {
+  } else if ('learningRoom' === rooms) {
+   document.getElementById('d').innerHTML =  'Learning Room 1: Avialable'
+    + '</br>Learning Room 2: Avialable </br> Learning Room 3: Not Avialable </br>'
+    + 'Learning Room 5: Avialable </br> Learning Room 6: Not Avialable';
+    return true;
+  }else if ('gamesRoom' === rooms) {
+    document.getElementById('d').innerHTML =  'Games Room 1: Avialable'
+    + '</br>Games Room 2: Avialable </br> Games Room 3: Not Avialable </br>'
+    + 'Games Room 5: Avialable </br> Games Room 6: Not Avialable';
     return gamesRoom;
-  } else if (room === 'quiet time') {
+  } else if ('quietTimeRoom' === rooms) {
+   document.getElementById('d').innerHTML =  'Quiet Time Room 1: Avialable'
+    + '</br>Quiet Time Room 2: Avialable </br> Quiet Time Room 3: Not Avialable </br>'
+    + 'Quiet Time Room 5: Avialable </br> Quiet Time Room 6: Not Avialable';
     return quietTime;
-  } else if (room === 'working room') {
+  } else if ('workingRoom' === rooms) {
+   document.getElementById('d').innerHTML =  'Working Room 1: Avialable'
+    + '</br>Working Room 2: Avialable </br> Working Room 3: Not Avialable </br>'
+    + 'Working Room 5: Avialable </br> Working Room 6: Not Avialable';
     return workingRoom;
   } else {
     return `No room named ${room}`
   }
-};
-
-getRoom ('meeting room')
+}
