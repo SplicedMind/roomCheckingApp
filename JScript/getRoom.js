0e28507a0297be08a272436c34e37bad69e4d6a3
Akinbode Femi <femi661@gmail.com>
2017-01-15 21:00:40 +0100

    Update to css and html files

--- JScript/getRoom.js
+++ JScript/getRoom.js
@@ -3,32 +3,37 @@ function getRoom() {
   var rooms = document.getElementById('s').value;
  
   if ('meetingRoom' === rooms) {
+    document.getElementById('er').innerHTML ="";
     document.getElementById('d').innerHTML =  'Meeting Room 1: Not Avialable'
     + '</br>Meeting Room 2: Avialable </br> Meeting Room 3: Not Avialable </br>'
     + 'Meeting Room 5: Avialable </br> Meeting Room 6: Not Avialable';
     return true;
   } else if ('learningRoom' === rooms) {
+    document.getElementById('er').innerHTML ="";
    document.getElementById('d').innerHTML =  'Learning Room 1: Not Avialable'
     + '</br>Learning Room 2: Avialable </br> Learning Room 3: Avialable </br>'
     + 'Learning Room 5: Not Avialable </br> Learning Room 6: Not Avialable';
     return true;
   }else if ('gamesRoom' === rooms) {
+    document.getElementById('er').innerHTML ="";
     document.getElementById('d').innerHTML =  'Games Room 1: Avialable'
     + '</br>Games Room 2: Avialable </br> Games Room 3: Not Avialable </br>'
     + 'Games Room 5: Avialable </br> Games Room 6: Avialable';
     return gamesRoom;
   } else if ('quietTimeRoom' === rooms) {
+    document.getElementById('er').innerHTML ="";
    document.getElementById('d').innerHTML =  'Quiet Time Room 1: Avialable'
     + '</br>Quiet Time Room 2: Not Avialable </br> Quiet Time Room 3: Not Avialable </br>'
     + 'Quiet Time Room 5: Not Avialable </br> Quiet Time Room 6: Avialable';
     return quietTime;
   } else if ('workingRoom' === rooms) {
+     document.getElementById('er').innerHTML ="";
    document.getElementById('d').innerHTML =  'Working Room 1: Avialable'
     + '</br>Working Room 2: Not Avialable </br> Working Room 3: Not Avialable </br>'
     + 'Working Room 5: Avialable </br> Working Room 6: Not Avialable';
     return workingRoom;
   } else {
-    document.getElementById('d').innerHTML =  'Please select a room category';
+    document.getElementById('er').innerHTML =  'Please select a room category';
     return true;
   }
 }
